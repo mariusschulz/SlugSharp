7bf9ff815c9ba5b8ff672f061ef2959678bd0e2b
Marius Schulz <marius.schulz@me.com>
2014-01-29 16:35:04 +0100

    Initial commit

---
+++ SlugSharp/Slug.fs
@@ -0,0 +1,13 @@
+﻿namespace SlugSharp
+open SlugTextSanitizer
+
+type Slug() = 
+    static member CreateFrom(input) =
+        input
+            |> trim
+            |> lowercase
+            |> substituteUmlautsAndAmpersands
+            |> replaceSpacesByHyphens
+            |> removeIllegalCharacters
+            |> mergeConsecutiveHyphens
+            |> removeTrailingHyphens
---
+++ SlugSharp/SlugTextSanitizer.fs
@@ -0,0 +1,28 @@
+﻿module internal SlugSharp.SlugTextSanitizer
+open System.Text.RegularExpressions
+    
+let trim (input:string) = input.Trim()
+let lowercase (input:string) = input.ToLower()
+
+let replace (oldChar:string) (newChar:string) (input:string) =
+    input.Replace(oldChar, newChar)
+
+let replaceAmpersands input = replace ("&") ("and") input
+let replaceSpacesByHyphens input = replace (" ") ("-") input
+
+let removeIllegalCharacters input = Regex.Replace(input, "[^-a-z0-9]", "")
+
+let substituteUmlautsAndAmpersands input =
+    let sanitizeChar = function
+        | 'ä' -> "ae"
+        | 'ö' -> "oe"
+        | 'ü' -> "ue"
+        | 'ß' -> "ss"
+        | '&' -> "and"
+        | c -> c.ToString()
+
+    String.concat "" (Seq.map sanitizeChar input)
+
+let mergeConsecutiveHyphens input = Regex.Replace(input, "-{2,}", "-")
+
+let removeTrailingHyphens (input:string) = input.TrimEnd('-')
