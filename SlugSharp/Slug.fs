025c2a2be8dbb20e128f24ba400f24faa144a432
Marius Schulz <marius.schulz@me.com>
2014-01-31 01:00:34 +0100

    Incorporated feedback from @sforkmann: Slug is now a module

--- SlugSharp/Slug.fs
+++ SlugSharp/Slug.fs
@@ -1,13 +1,38 @@
-﻿namespace SlugSharp
-open SlugTextSanitizer
+﻿module public Slug
+open System.Text.RegularExpressions
+    
+let private trim (input:string) = input.Trim()
+let private lowercase (input:string) = input.ToLower()
 
-type Slug = 
-    static member CreateFrom(input) =
-        input
-            |> trim
-            |> lowercase
-            |> substituteUmlautsAndAmpersands
-            |> replaceSpacesByHyphens
-            |> removeIllegalCharacters
-            |> mergeConsecutiveHyphens
-            |> removeTrailingHyphens
+let private replace (oldChar:string) (newChar:string) (input:string) =
+    input.Replace(oldChar, newChar)
+
+let private replaceAmpersands = replace "&" "and"
+let private replaceSpacesByHyphens = replace " " "-"
+
+let private removeIllegalCharacters input = Regex.Replace(input, "[^-a-z0-9]", "")
+
+let private substituteUmlautsAndAmpersands input =
+    let sanitizeChar = function
+        | 'ä' -> "ae"
+        | 'ö' -> "oe"
+        | 'ü' -> "ue"
+        | 'ß' -> "ss"
+        | '&' -> "and"
+        | c -> c.ToString()
+
+    Seq.map sanitizeChar input |> String.concat ""
+
+let private mergeConsecutiveHyphens input = Regex.Replace(input, "-{2,}", "-")
+
+let private removeTrailingHyphens (input:string) = input.TrimEnd('-')
+
+let CreateFrom input =
+    input
+        |> trim
+        |> lowercase
+        |> substituteUmlautsAndAmpersands
+        |> replaceSpacesByHyphens
+        |> removeIllegalCharacters
+        |> mergeConsecutiveHyphens
+        |> removeTrailingHyphens
